97c2e70ab1b23b24e0fdc6d1151bf64abc457fd1
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-09-14 18:53:58 +0200

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -13,10 +13,7 @@ local Translations = {
     }
 }
 
-if GetConvar('qb_locale', 'en') == 'fr' then
-    Lang = Locale:new({
-        phrases = Translations,
-        warnOnMissing = true,
-        fallbackLang = Lang,
-    })
-end
+Lang = Lang or Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
