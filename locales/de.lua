71967c4c2818e658fc04067e5a44618558eb3eab
Marcell <chillcode.codersparadise@gmail.com>
2024-09-12 16:02:54 +0200

    added locale de.lua

---
+++ locales/de.lua
@@ -0,0 +1,21 @@
+local Translations = {
+    error = {
+        invalid_ext = "Das ist keine gültige Erweiterung, nur",
+        allowed_ext = "Erweiterungs-Links sind erlaubt.",
+    },
+    info = {
+        use_printer = "Drucker verwenden",
+    },
+    command = {
+        spawn_printer = "Drucker spawnen",
+    }
+}
+
+
+if GetConvar('qb_locale', 'en') == 'de' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
